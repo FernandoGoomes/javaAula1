e14740210c769e607ef99d00580171226f8bd5c4
Fernando Marsicano Gomes <fmarsica@everis.com>
2020-10-21 13:25:45 -0300

    implementando if,case,for,while

--- src/javaAula1/testeAula1.java
+++ src/javaAula1/testeAula1.java
@@ -4,7 +4,62 @@ public class testeAula1 {
 
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo, Fernando !!");
+		
+	//Estrutura If else
+		
+		//	boolean valor = false;
+		
+	//	if (valor == true) {
+	//		System.out.println("Verdadeiro");
+	//	}
+	//	else {
+	//		System.out.println("falso");
+	//	}
  
+	
+		//Estrutura While
+		
+	//	int valor = 0;
+	//	while (valor < 5) {
+	//		valor++;
+	//		System.out.println(valor);
+	//	}
+		
+	// Estrutura for
+		
+	//	for (int i = 0; i < 5; i++) {
+	//		System.out.println(i);
+	//	}
+		
+		
+		int day = 7;
+		switch (day) {
+		  case 1:
+		    System.out.println("Domingo");
+		    break;
+		  case 2:
+		    System.out.println("Segunda");
+		    break;
+		  case 3:
+		    System.out.println("Terça");
+		    break;
+		  case 4:
+		    System.out.println("Quarta");
+		    break;
+		  case 5:
+		    System.out.println("Quinta");
+		    break;
+		  case 6:
+		    System.out.println("Sexta");
+		    break;
+		  case 7:
+		    System.out.println("Sabado");
+		    break;
+		  default:
+	           System.out.println ("O valor da variavel não é nenhum dos anteriores");
+		}
+		
+		
 	}
 
 }
